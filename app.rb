8a11e2b7317c47683ffcf5a2dcb79819cce8151e
Hamilton Karl <hamilton@nonfatmedia.com>
2015-06-12 15:44:14 -0700

    formtastic

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require 'rubygems'
 require 'sinatra'
-configure { set :server, :puma }
+require "active_record"
 require './routes'
+configure { set :server, :puma }
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'sqlite3',
+	:database =>  'sinatra_application.sqlite3.db'
+	)
--- routes.rb
+++ routes.rb
@@ -1,4 +1,15 @@
 
+
 get '/' do 
  erb :index
+end
+
+get '/register/?' do
+	erb :register
+end
+
+get '/test' do
+	@color = SecureRandom.hex(n=6)[0..5]
+	@blackbox = Random.rand(1..10).to_i
+	erb :test
 end
