a1df8a8443abbc30c8be34b02f3c447db84c776b
Hamilton Karl <hamilton@nonfatmedia.com>
2015-11-18 13:43:41 -0800

    test config

--- test/controllers/transmissions_controller_test.rb
+++ test/controllers/transmissions_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TransmissionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 	test "truthyness" do
+ 		assert(true)
+ 	end
+
 end
